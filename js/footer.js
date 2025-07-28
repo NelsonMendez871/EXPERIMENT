400629ebab950ba1cf1019bb86e81098bffcfb1c
NelsonMendez871 <NelsonMendez871@users.noreply.github.com>
2025-07-28 18:23:57 -0500

    Actualización del proyecto: cambios recientes

--- js/footer.js
+++ js/footer.js
@@ -1,11 +1,8 @@
-// js/footer.js
 document.addEventListener('DOMContentLoaded', function () {
   const placeholder = document.getElementById('footer-placeholder');
   if (!placeholder) return;
 
-  const basePath = location.pathname.includes('/productos/') ? '../' : '';
-
-  fetch(`${basePath}../../components/footer.html`)
+  fetch('/EXPERIMENT/components/footer.html')
     .then(response => response.text())
     .then(data => {
       placeholder.innerHTML = data;
